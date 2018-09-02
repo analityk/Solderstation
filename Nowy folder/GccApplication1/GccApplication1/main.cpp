37b503e7a29087e0b50062a17f84256a0f052ac7
Szymon Romanowski <romanowskis1at@interia.pl>
2018-09-02 21:09:39 +0200

    asd
    d

---
+++ Nowy folder/GccApplication1/GccApplication1/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.cpp
+ *
+ * Created: 2018-09-02 21:08:41
+ * Author : Szymon
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
